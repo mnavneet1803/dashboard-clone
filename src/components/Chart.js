17dc0024e10ab218e557324f1b1c339695c300a8
mnavneet1803 <mnavneet1803@gmail.com>
2024-08-23 12:49:59 +0530

    Chart Updated

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,46 +1,34 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
-import { Button } from "react-bootstrap";
 import { MdOutlineFileDownload } from "react-icons/md";
+
 const Chart = ({ hw }) => {
-
-
     let data = [
         { Topic: 'Salary', Value: 2200 },
         { Topic: 'Freelance', Value: 1500 },
         { Topic: 'Investment', Value: 3000 },
-    ]
+    ];
 
     const [showDownloadButton, setShowDownloadButton] = useState(false);
     const [backgroundStyle, setBackgroundStyle] = useState({ position: 'relative', radius: "50px" });
 
-
     const svgRef = useRef();
-
 
     const handleDownload = () => {
         const svgNode = svgRef.current;
-
-        // Create a canvas element to render the SVG content
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
         const svgString = new XMLSerializer().serializeToString(svgNode);
 
-        // Set canvas background color to white
         canvas.width = svgNode.clientWidth;
         canvas.height = svgNode.clientHeight;
-        ctx.fillStyle = '#ffffff'; // white color
+        ctx.fillStyle = '#ffffff';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // Create an image from the SVG content
         const img = new Image();
         img.onload = () => {
-            // Draw the image onto the canvas
             ctx.drawImage(img, 0, 0);
-
-            // Convert the canvas to a data URL
             const imageURL = canvas.toDataURL();
-            // Trigger download
             const link = document.createElement('a');
             link.href = imageURL;
             link.download = 'Articles_Over_Category.png';
@@ -48,18 +36,21 @@ const Chart = ({ hw }) => {
         };
         img.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgString)));
     };
-    const margin = { top: 20, right: 40, bottom: 40, left: Math.round(hw.width / 4.9) < 260 ? 70 : 100 };
-    const width = (Math.round(hw.width / 4.9) > 100 ? Math.round(hw.width / 4.6) : 100) - margin.left - margin.right;
-    const height = (Math.round(hw.height / 3.4) > 200 ? Math.round(hw.height / 3.4) : 200) - margin.top - margin.bottom;
 
+    const calculateDimensions = () => {
+        const margin = { top: 20, right: 40, bottom: 40, left: Math.round(hw.width / 4.9) < 260 ? 70 : 100 };
+        const minWidth = 300; // Set a minimum width for mobile
+        const calculatedWidth = Math.max(Math.round(hw.width / 4.6), minWidth) - margin.left - margin.right;
+        const calculatedHeight = (Math.round(hw.height / 3.4) > 200 ? Math.round(hw.height / 3.4) : 200) - margin.top - margin.bottom;
+
+        return { width: calculatedWidth, height: calculatedHeight, margin };
+    };
 
     useEffect(() => {
-
+        const { width, height, margin } = calculateDimensions();
 
         const svg = d3.select(svgRef.current);
-        if (!svg.select("svg").empty()) {
-            svg.select("svg").remove();
-        }
+        svg.selectAll("*").remove(); // Clear previous SVG content
 
         const svgContainer = svg
             .append("svg")
@@ -68,70 +59,35 @@ const Chart = ({ hw }) => {
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
+        const getHighestValue = (array) => array.reduce((max, item) => Math.max(max, item.Value), -Infinity);
 
-
-        function getHighestValue(array) {
-            let maxValue = -Infinity;
-
-            array.forEach((item) => {
-                if (item.Value && !isNaN(item.Value)) {
-                    const value = parseInt(item.Value);
-                    maxValue = Math.max(maxValue, value);
-                }
-            });
-
-            return maxValue;
-        }
-        const x = d3
-            .scaleLinear()
+        const x = d3.scaleLinear()
             .domain([0, getHighestValue(data)])
             .range([5, width < 130 ? 120 : width]);
-        svgContainer
-            .append("g")
+
+        svgContainer.append("g")
             .attr("transform", `translate(0,${height})`)
-            .call(
-                d3.axisBottom(x).ticks(4) // Adjust the outer tick size
-            )
+            .call(d3.axisBottom(x).ticks(4))
             .style("font-size", hw.width < 1400 ? "12px" : "15px")
             .selectAll(".domain, .tick line")
             .remove();
 
-        // Y axis
-        const y = d3
-            .scaleBand()
+        const y = d3.scaleBand()
             .range([0, data.length < 3 ? height - 80 : height])
             .domain(data.map((d) => d.Topic))
             .padding(0.2);
-        svgContainer
-            .append("g")
+
+        svgContainer.append("g")
             .call(d3.axisLeft(y))
             .style("font-size", hw.width < 1400 ? "12px" : "15px")
             .selectAll(".domain, .tick line")
             .remove();
 
-        const subgroups = [
-            "Technical",
-            "billing",
-            "support",
-            "bugs",
-            "urgent",
-            "performance",
-        ];
+        const color = d3.scaleOrdinal()
+            .domain(data.map(d => d.Topic))
+            .range(["#0384fc", "#a437ed", "#cc37ed", "#edad37", "#43c5e6", "#26994e"]);
 
-        const color = d3
-            .scaleOrdinal()
-            .domain(subgroups)
-            .range([
-                "#0384fc",
-                "#a437ed",
-                "#cc37ed",
-                "#edad37",
-                "#43c5e6",
-                "#26994e",
-            ]);
-        // Bars
-        svgContainer
-            .selectAll("rect")
+        svgContainer.selectAll("rect")
             .data(data)
             .enter()
             .append("rect")
@@ -139,22 +95,20 @@ const Chart = ({ hw }) => {
             .attr("y", (d) => y(d.Topic))
             .attr("width", (d) => x(d.Value))
             .attr("height", y.bandwidth() - 2)
-            .attr("rx", 3) // Set horizontal radius
-            .attr("ry", 20) // Set vertical radius
+            .attr("rx", 3)
+            .attr("ry", 20)
             .attr("fill", (d) => color(d.Topic));
 
-        // Text inside bars
-        svgContainer
-            .selectAll(".bar-label")
+        svgContainer.selectAll(".bar-label")
             .data(data)
             .enter()
             .append("text")
             .attr("class", "bar-label")
             .attr("x", (d) => {
                 if (parseInt(d.Value) < 1000) {
-                    return x(d.Value) + 7; // Position outside the bar
+                    return x(d.Value) + 7;
                 } else {
-                    return x(d.Value) - 50; // Position inside the bar
+                    return x(d.Value) - 50;
                 }
             })
             .attr("y", (d) => y(d.Topic) + y.bandwidth() / 2)
@@ -162,15 +116,13 @@ const Chart = ({ hw }) => {
             .attr("dy", ".25em")
             .attr("fill", (d) => {
                 if (parseInt(d.Value) < 1000) {
-                    return color(d.Topic); // Bar color for smaller values
+                    return color(d.Topic);
                 } else {
-                    return "white"; // White color for labels inside bars
+                    return "white";
                 }
             })
             .style("font-size", hw.width < 1400 ? "15px" : "18px");
     }, [data, hw]);
-
-
 
     return (
         <div
@@ -183,22 +135,13 @@ const Chart = ({ hw }) => {
                 setShowDownloadButton(false)
                 setBackgroundStyle({ position: 'relative', radius: "50px" })
             }}
-
         >
             <svg
-                width={(width + margin.left + margin.right) < 333 ? 333 :width + margin.left + margin.right} // Set width and height here
-                height={height + margin.top + margin.bottom}
+                width={calculateDimensions().width + calculateDimensions().margin.left + calculateDimensions().margin.right}
+                height={calculateDimensions().height + calculateDimensions().margin.top + calculateDimensions().margin.bottom}
                 ref={svgRef}
-                style={backgroundStyle}
-            >
-                <g
-                    transform={`translate(${hw.width / 12},${hw.width / 12})`}
-                >
-                </g>
-            </svg>
+            />
 
-
-            {/* Button to trigger download */}
             {showDownloadButton && (
                 <button
                     className="btn"
@@ -217,7 +160,6 @@ const Chart = ({ hw }) => {
             )}
         </div>
     );
-
 };
 
 export default Chart;
